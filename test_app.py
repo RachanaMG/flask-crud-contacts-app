dd4787f61832ff3db5e56a039ae1fd741273f3f6
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-17 14:27:47 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,33 +5,6 @@ from flask_testing import TestCase
 from app import app
 
 # Import your Flask app here
-app = Flask(__name__)
-
-# Mysql Connection
-if os.environ.get('MYSQL_HOST'):
-    app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-else:
-    app.config['MYSQL_HOST'] = 'localhost'
-
-if os.environ.get('MYSQL_USER'):
-    app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
-else:
-    app.config['MYSQL_USER'] = 'root'
-
-if os.environ.get('MYSQL_PASSWORD'):
-    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
-else:
-    app.config['MYSQL_PASSWORD'] = 'root'
-
-if os.environ.get('MYSQL_DB'):
-    app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
-else:
-    app.config['MYSQL_DB'] = 'flaskcrud'
-
-mysql = MySQL(app)
-
-# settings
-app.secret_key = "mysecretkey"
 
 class AppTestCase(TestCase):
 def create_app(self):
@@ -60,23 +33,6 @@ self.assert200(response)
 def test_add_contact(self):
 response = self.client.post('/add_contact', data={'fullname': 'John', 'phone': '123456789', 'email': 'john@example.com'})
 self.assertRedirects(response, '/')
-
-# Add assertions to verify that the contact was added to the database
-
-def test_get_contact(self):
-# Add test data to the database
-# Perform a GET request to '/edit/<id>'
-# Add assertions to verify the rendered template and data
-
-def test_update_contact(self):
-# Add test data to the database
-# Perform a POST request to '/update/<id>'
-# Add assertions to verify that the contact was updated in the database
-
-def test_delete_contact(self):
-# Add test data to the database
-# Perform a POST request to '/delete/<id>'
-# Add assertions to verify that the contact was deleted from the database
 
 if __name__ == '__main__':
 unittest.main()
