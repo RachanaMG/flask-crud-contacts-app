3e46af6cd03fccaa6ba4736682d6246655568a1a
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-17 10:34:12 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,54 @@
+import unittest
+from flask import Flask, template_rendered
+from flask_mysqldb import MySQL
+from flask_testing import TestCase
 
+# Import your Flask app here
+
+class AppTestCase(TestCase):
+def create_app(self):
+app = Flask(__name__)
+app.config['TESTING'] = True
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flaskcrud'
+app.secret_key = "mysecretkey"
+return app
+
+def setUp(self):
+# Set up any necessary database fixtures or test data
+pass
+
+def tearDown(self):
+# Clean up after each test
+pass
+
+def test_index(self):
+response = self.client.get('/')
+self.assert200(response)
+# Add assertions to verify the response data
+
+def test_add_contact(self):
+response = self.client.post('/add_contact', data={'fullname': 'John', 'phone': '123456789', 'email': 'john@example.com'})
+self.assertRedirects(response, '/')
+
+# Add assertions to verify that the contact was added to the database
+
+def test_get_contact(self):
+# Add test data to the database
+# Perform a GET request to '/edit/<id>'
+# Add assertions to verify the rendered template and data
+
+def test_update_contact(self):
+# Add test data to the database
+# Perform a POST request to '/update/<id>'
+# Add assertions to verify that the contact was updated in the database
+
+def test_delete_contact(self):
+# Add test data to the database
+# Perform a POST request to '/delete/<id>'
+# Add assertions to verify that the contact was deleted from the database
+
+if __name__ == '__main__':
+unittest.main()
